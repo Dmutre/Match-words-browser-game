3ad6029c208e656b00e10ebe96ab2e7276cbc405
Dmutre <dmytro.lesko@gmail.com>
2023-05-28 21:18:03 +0200

    Update GameLobby.js

--- GameLobby/GameLobby.js
+++ GameLobby/GameLobby.js
@@ -10,19 +10,24 @@ document.getElementById('sendButton').addEventListener('click', function() {
 });
 
 document.getElementById('inputText').addEventListener('keypress', function(e) {
+  const inputText = this; // Зберегти посилання на елемент inputText
+
   if (e.key === 'Enter') {
-    const inputText = this.value;
+    const inputValue = inputText.value;
 
     fetch('/postText', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ text: inputText }),
+      body: JSON.stringify({ text: inputValue }),
     })
       .then(response => response.json())
       .then(result => {
         console.log('Отримано результат від сервера:', result);
+        if (result.success === true) {
+          inputText.value = ''; // Очистити поле вводу
+        }
       })
       .catch(error => {
         console.log('Сталася помилка:', error);
