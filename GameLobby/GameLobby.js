1b7d9e4a09ec4e229c589969b1f086ffed8d1517
Dmutre <dmytro.lesko@gmail.com>
2023-05-29 14:23:58 +0200

    0.5
    Now it automatically send word and in some time send another part of words. There I added and changed code mostly in gameLobby

--- GameLobby/GameLobby.js
+++ GameLobby/GameLobby.js
@@ -1,36 +1,80 @@
-document.getElementById('sendButton').addEventListener('click', function() {
+'use strict';
+
+document.addEventListener('DOMContentLoaded', initialFunction);
+const messageWindow = document.getElementById('messageWindow');
+const inputWord = document.getElementById('inputWord');
+const timeToCheck = 10000;//Time to input word before it change and count as unfound
+let timer;
+
+inputWord.addEventListener('keydown', (e) => {if(e.key === 'Enter') sendWord()});
+
+function initialFunction(){
+  wordReq();
+  startTimer();
+}
+
+function wordReq(){
   fetch('/getWord')
     .then(response => response.json())
     .then(data => {
-      document.getElementById('messageWindow').innerText = data.word;
+      messageWindow.innerText = data.word;
     })
     .catch(error => {
       console.log('Помилка при отриманні слова:', error);
     });
-});
+}
 
-document.getElementById('inputText').addEventListener('keypress', function(e) {
-  const inputText = this; // Зберегти посилання на елемент inputText
+function startTimer() {
+  clearTimeout(timer);
+  timer = setTimeout(() => {
+    console.log('Таймер завершився. Ти невстиг!');
+    borderColor(false);
+    wordReq();
+    startTimer();
+  }, timeToCheck);
+}
 
-  if (e.key === 'Enter') {
-    const inputValue = inputText.value;
+function borderColor(isCorrect) {
+  const inputBorder = inputWord.style;
 
-    fetch('/postText', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ text: inputValue }),
-    })
-      .then(response => response.json())
-      .then(result => {
-        console.log('Отримано результат від сервера:', result);
-        if (result.success === true) {
-          inputText.value = ''; // Очистити поле вводу
-        }
-      })
-      .catch(error => {
-        console.log('Сталася помилка:', error);
-      });
+  if (isCorrect) {
+    inputBorder.borderColor = 'green';
+    inputBorder.setProperty('--input-border-opacity', '1');
+    inputWord.value = '';
+  } else {
+    inputBorder.borderColor = 'red';
+    inputBorder.setProperty('--input-border-opacity', '1');
+    inputWord.classList.add('shake-animation');
+    inputWord.addEventListener('animationend', removeAnimation);
   }
-});
+}
+
+function removeAnimation() {
+  inputWord.classList.remove('shake-animation');
+}
+
+function sendWord(){
+  const inputValue = inputWord.value;
+
+  fetch('/postText', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ text: inputValue }),
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log('Отримано результат від сервера:', result);
+      if (result.success === true) {
+        wordReq();
+        startTimer();
+        borderColor(true);
+      } else{
+        borderColor(false);
+      }
+    })
+    .catch(error => {
+      console.log('Сталася помилка:', error);
+  });
+}
