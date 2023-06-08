dba7b359d73668f4555ba40286e858efc8ca0dbe
Dmutre <dmytro.lesko@gmail.com>
2023-06-08 21:20:06 +0200

    Update server.js

--- scriptsJS/server.js
+++ scriptsJS/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const path = require('node:path');
-const tools = require('./tools');
 const express = require('express');
+const { getRandWordPart, dictionaryManager, gameLobbyParameters, } = require('./tools');
 const app = express();
 const PORT = 3000;
 
@@ -21,26 +21,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/gameLobby', async (req, res) => {
-  let language = tools.gameLobbyParameters.get().language;
-  await tools.dictionaryManager.readDictionary(dictionaryPath, language);
+  let language = gameLobbyParameters.get().language;
+  await dictionaryManager.readDictionary(dictionaryPath, language);
   res.sendFile(path.join(gameLobbyPath, 'GameLobby.html'));
 });
 
 app.get('/getWord', (req, res) => {
-  const word = tools.getRandWordPart(); 
+  const word = getRandWordPart();
   res.json({ word: word });
 });
 
 app.post('/getParameters', (req, res) => {
   let parameters = req.body;
-  tools.gameLobbyParameters.set(parameters);
+  gameLobbyParameters.set(parameters);
   res.json({ ok: true });
 });
 
 app.post('/postText', (req, res) => {
   let text = req.body.text;
   console.log('Отримано текст з клієнта:', text);
-  if(tools.dictionaryManager.acceptWord(text)){
+  if(dictionaryManager.acceptWord(text)){
     res.json({ success: true });
   } else res.json({ success: false });
 });
